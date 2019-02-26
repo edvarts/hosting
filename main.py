e6aa2cd0de9a4318e70eb684573eb432403af478
Edvarts <edvardsemersons@inbox.lv>
2019-02-26 20:25:20 +0200

    this vsup

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, SmartNinja!"
-
+   return render_template('gugl.html')
+@app.route("/learn")
+def learn():
+    return "<h1>es esmu kruts zens</h1> " \
+           "<h2>tapec macos progot</h2>"
 
 if __name__ == '__main__':
     app.run()
