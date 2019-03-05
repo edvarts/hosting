8af794af14d73818603c002057c5bdb3c2ba2f5f
Edvarts <edvardsemersons@inbox.lv>
2019-03-05 21:07:24 +0200

    ckkies

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
 
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
+
+@app.route("/about-me", methods=["GET"])
+def about():
+    return render_template("about.html")
 @app.route("/")
 def index():
     personname = 'EDE'
