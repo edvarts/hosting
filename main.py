ff32847f14d2e79eada3b91d419f02c0de7b7eb9
Edvarts <edvardsemersons@inbox.lv>
2019-03-12 00:38:36 +0200

    newdiss

--- main.py
+++ main.py
@@ -2,17 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/contact", methods=["POST"])
-def contact():
-    contact_name = request.form.get("contact-name")
-    contact_email = request.form.get("contact-email")
-    contact_message = request.form.get("contact-message")
 
-    print(contact_name)
-    print(contact_email)
-    print(contact_message)
-
-    return render_template("success.html")
 
 @app.route("/about-me", methods=["GET"])
 def about():
@@ -20,7 +10,7 @@ def about():
 @app.route("/")
 def index():
     personname = 'EDE'
-    return render_template('gugl.html')
+    return render_template('index.html')
 @app.route("/learn")
 def learn():
     return "<h1>es esmu kruts zens</h1> " \
